68d3cc8db10177ed342dd5ab55180713664f9024
Mike <mike@graywind.org>
2024-05-17 20:58:28 -0500

    Update neon_homeassistant_skill/__init__.py

--- neon_homeassistant_skill/__init__.py
+++ neon_homeassistant_skill/__init__.py
@@ -227,7 +227,7 @@ class NeonHomeAssistantSkill(OVOSSkill):
             if self.verbose:
                 self.speak_dialog("acknowledge")
             else:
-                self.log.info("Trying to turn off device")
+                self.log.info(f"Trying to turn off device {device}")
         else:
             self.speak_dialog("no.parsed.device")
 
